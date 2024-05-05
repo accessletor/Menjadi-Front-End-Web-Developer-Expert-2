82a90ed124f1ebcfacca5e2b12bf2bf8e4543031
asep <githubasep@gmail.com>
2024-05-05 19:36:04 +0700

    first commit

---
+++ src/scripts/views/templates/resto-card.js
@@ -0,0 +1,19 @@
+import CONFIG from '../../global/config';
+
+const restoCard = (resto) => `
+<div tabindex =0 class="card">
+    <div class="icon" tabindex="0">
+        <span class="rate" tabindex="0"><p>★ ${resto.rating}</p></span>
+        <img src="${CONFIG.BASE_IMAGE_URL + resto.pictureId}" alt="${resto.name}">
+    </div>
+    <div class="text" tabindex="0">
+        <p class="title-city">${resto.name} - ${resto.city}</p>
+        <p class="description">${resto.description}</p>
+    </div>
+    <a href="#/resto/${resto.id}" class="detail"><div tabindex="0">
+        <p><strong>Detail</strong></p>
+    </div></a>
+</div>
+`;
+
+export default restoCard;
